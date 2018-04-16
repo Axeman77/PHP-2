37b5a68ed88f8fb8e59bcda03ad6181d6aeaf061
c205 <axeman@bk.ru>
2018-04-16 15:39:09 +0300

    HW1-task 4...6

--- public/index.php
+++ public/index.php
@@ -38,7 +38,7 @@ class Product{
     }
 
 
-    private function prepareImage()
+    protected function prepareImage()
     {
         return "<img src= '{$this->image}' alt='image' width='200'>";
     }
@@ -60,7 +60,7 @@ class Card extends Product {
 
     public function __construct($id = null, $name = null, $description = null, $image= null, $price=null)
     {
-        parent::__construct($id, $name,$description, $image, $price);
+        parent::__construct($id, $name, $description, $image);
         $this->price = $price;
     }
 
@@ -76,9 +76,12 @@ class Card extends Product {
         echo "<p>{$this->price}$</p>";
     }
 
+    protected function prepareImage()
+    {
+        return "<img src= '{$this->image}' alt='image'>";
+    }
 
 }
-
 
 $product = new Product(1, "Casio", "Наручные часы", "img/model_1.jpg");
 $product->getProductsList();
@@ -88,3 +91,50 @@ $product2->getProductsList();
 
 $card = new Card(2, "Casio2", "Наручные часы2", "img/model_2.jpg", 100);
 $card->display();
+
+/*class A {
+    public function foo() {
+        static $x = 0; // переменная объявлена в контектсте класса общая для всех с дефолтным значением 0;
+        echo ++$x;
+    }
+}
+$a1 = new A(); // создаем экземпляр объекта класса А
+$a2 = new A(); // создаем еще один экземпляр объекта класса А
+$a1->foo(); // вызываем метод класса А для объекта $a1 и значение $х становится = 1
+$a2->foo(); // вызываем метод класса А для объекта $a2 и значение $х становится = 2
+$a1->foo(); // снова вызываем метод класса А для объекта $a1 и значение $х еще раз переписывается становится = 3
+$a2->foo(); // то же для  объекта $a2 и значение $х становится = 4
+// все делается в контксте класса А(вне контекста объектов),  для разных объектов
+
+class A {
+    public function foo() {
+        static $x = 0; // переменная объявлена в контектсте класса общая для всех с дефолтным значением 0;
+        echo ++$x;
+    }
+}
+class B extends A { // здесь своя переменная  в контектсте класса общая для всех с дефолтным значением 0;
+}
+$a1 = new A(); // создаем экземпляр объекта класса А
+$b1 = new B(); // создаем экземпляр объекта класса B
+$a1->foo(); // вызываем метод класса А для объекта $a1 и значение $х становится = 1
+$b1->foo(); // вызываем метод класса B (в контексте класса В) для объекта $b1 и значение $х становится = 1
+$a1->foo(); // еще раз вызываем метод класса А (в контексте класса А)для объекта $a1 и значение $х становится = 2
+$b1->foo(); // еще раз вызываем метод класса B (в контексте класса B)для объекта $b1 и значение $х становится = 2
+
+
+// так как мы не передаем никаких параметров конструктору, то скобки можно не ставить, результат будет тот же
+class A {
+    public function foo() {
+        static $x = 0;
+        echo ++$x;
+    }
+}
+class B extends A {
+}
+$a1 = new A;
+$b1 = new B;
+$a1->foo();
+$b1->foo();
+$a1->foo();
+$b1->foo();
+*/
